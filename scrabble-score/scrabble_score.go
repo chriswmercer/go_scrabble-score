c2f51758484ba3bef113ce8c1c895b2465e58d0f
chriswmercer <chriswm@me.com>
2019-01-03 13:17:42 +0000

    Add files via upload

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,44 @@
+//Package scrabble provides a function to score words for scrabble
+package scrabble
+
+import "unicode"
+
+var scoreKey = map[rune]int{
+	'A': 1,
+	'B': 3,
+	'C': 3,
+	'D': 2,
+	'E': 1,
+	'F': 4,
+	'G': 2,
+	'H': 4,
+	'I': 1,
+	'J': 8,
+	'K': 5,
+	'L': 1,
+	'M': 3,
+	'N': 1,
+	'O': 1,
+	'P': 3,
+	'Q': 10,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'U': 1,
+	'V': 4,
+	'W': 4,
+	'X': 8,
+	'Y': 4,
+	'Z': 10,
+}
+
+//Score calculates the scrabble score for a given input string
+func Score(input string) int {
+	score := 0
+
+	for _, r := range input {
+		score += scoreKey[unicode.ToUpper(r)]
+	}
+
+	return score
+}
